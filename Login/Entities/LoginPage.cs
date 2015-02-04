98c5772ad73e132f21f52349f8b017982ad6f475
rolah <nick1@freemail.hu>
2015-02-04 15:03:20 +0100

    Selenium Login test

---
+++ Login/Entities/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Login.Entities
+{
+    class LoginPage
+    {
+        [FindsBy(How = How.Id, Using = "Email")]
+        private IWebElement emailFailed;
+
+        [FindsBy(How = How.Id, Using = "Passwd")]
+        private IWebElement passwordField;
+
+        [FindsBy(How = How.Id, Using = "signIn")]
+        private IWebElement loginButton;
+
+        public LoginPage(IWebDriver driver) 
+        {
+            PageFactory.InitElements(driver, this);                
+        }
+
+        public void login(string email, string password)
+        {
+            emailFailed.SendKeys(email);
+            passwordField.SendKeys(password);
+            loginButton.Click();
+        }
+    }
+}
---
+++ Login/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using Login.Entities;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/&hl=hu");
+
+            LoginPage loginPage = new LoginPage(driver);
+
+            loginPage.login("rolah76@gmail.com", "Helloka17");
+
+            Console.ReadLine();
+        }
+    }
+}
